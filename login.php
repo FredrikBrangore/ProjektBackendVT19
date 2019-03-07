69bb2cbdfceecdf5f78d673913650a4c3f46f8ae
Fredrik Vallez Ohlden <freddevv@gmail.com>
2019-03-07 10:40:17 +0100

    Bort med Seriebutik!

--- login.php
+++ login.php
@@ -33,62 +33,28 @@ require_once 'header.php';
 // 2. Arbeta med data som skickas via formuläret
 if ($_SERVER['REQUEST_METHOD'] == 'POST'):
     // Hämta värden (values) från olika input-fält
-    $name = $_POST['name'];
-    $pass = $_POST['pass'];
-    $tel = $_POST['tel'];
     $email = $_POST['email'];
-    $adress = $_POST['adress'];
+    $pass = $_POST['pass'];
 
     // Rensa data
-    $name = filter_var($name, FILTER_SANITIZE_STRING);
-    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
-    $tel = filter_var($tel, FILTER_SANITIZE_STRING);
     $email = filter_var($email, FILTER_SANITIZE_STRING);
-    $adress = filter_var($adress, FILTER_SANITIZE_STRING);
+    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
 
     // Validera data (Backend validering)
-    if (empty($name) or empty($tel) or empty($email) or empty($adress) or empty($pass)) {
+    if (empty($email) or empty($pass)) {
         echo '<div class="alert alert-danger">
-			    Du får inte ha tomma fält!</div>';
-    } elseif (strlen($name) < 3 or strlen($name) > 50) {
+				    Du får inte ha tomma fält!</div>';
+    } elseif (strpos($email, '@gmail.com') == false or strlen($email) > 100) {
     echo '<div class="alert alert-danger">
-    Namnet måste vara mer än 2 tecken eller max 50.
-    </div>';
-} elseif (strlen($tel) < 3 or strlen($tel) > 50) {
-    echo '<div class="alert alert-danger">
-    Telefon måste vara mer än 2 tecken eller max 50.
+    That Email does not exist if it should check spelling.
     </div>';
 } elseif (strlen($pass) < 3 or strlen($pass) > 25) {
     echo '<div class="alert alert-danger">
-    Password måste vara mer än 2 tecken eller max 50.
-    </div>';
-} elseif (strpos($email, '@gmail.com') == false or strlen($email) > 100) {
-    echo '<div class="alert alert-danger">
-    Du får bara ha gmail och max 100 tecken.
-    </div>';
-} elseif (strlen($adress) < 3 or strlen($adress) > 100) {
-    echo '<div class="alert alert-danger">
-    Adress måste vara mer än 2 tecken eller max 100.
+    That password is wrong please try again.
     </div>';
 } else {
     // Logga in i databasen
     require_once 'db.php';
-    if (condition) {
-        # code...
-    } else {
-        // Förbered en SQL-sats
-        $stmt = $conn->prepare("INSERT INTO contacts (name,tel,pass,email,adress)
-                                    VALUES (:name, :tel, :pass, :email, :adress) ");
-        // Binda variabler till params, som finns i VALUES()
-        $stmt->bindParam(':name', $name);
-        $stmt->bindParam(':tel', $tel);
-        $stmt->bindParam(':pass', $pass);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':adress', $adress);
-        // Skicka SQL till databasen
-        $stmt->execute();
-        header('Location: storepage.php');
-    }
 
 } // Avlusta if som validerar data
 endif;
